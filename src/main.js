4ab1ff7b344c1e31de589b000657670a5554da6d
susaneafonso <susaneafonso@users.noreply.github.com>
2023-11-14 15:40:34 -0300

    consulta lista de produtos funcionando

--- src/main.js
+++ src/main.js
@@ -1,72 +1,86 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-const api = axios.create({
-    baseURL: "https://tesstw.onrender.com",
+    const api = axios.create({
+        baseURL: "https://tesstw.onrender.com",
+    });
+
+    const dpai = "linhas";
+    const linhas_prod = document.querySelector(`.${dpai}`);
+    // console.log("linhas_prod:", linhas_prod);
+
+    async function consultar() {
+        try {
+            if (!linhas_prod) {
+                console.error(`Element with class "${dpai}" not found in the document.`);
+                return;
+            }
+
+            const response = await api.get('/produtos');
+            const dados = response.data.result;
+
+            // data.forEach(element => {
+                for (let i = 0; i < Math.min(dados.length, 21); i++) {
+                
+                const element = dados[i];
     
-})
+                const linhas = document.createElement("div");
+                linhas.setAttribute("class", "linhas");
 
+                const id = document.createElement("div");
+                id.setAttribute("class", "id");
+                id.innerHTML = element.cod;
+                linhas.appendChild(id);
 
-async function consultar() {
-  
-    const response = await api.get('/produtos')
-    const data=response.data.result
-    console.log(data)
-    
-    data.forEach(element => {
+                const nome = document.createElement("div");
+                nome.setAttribute("class", "nome");
+                nome.innerHTML = element.nome; 
+                linhas.appendChild(nome);
 
-         const linhas_prod= document.getElementsByClassName("linhas_prod")
-         const linhas= document.createElement("div")
-         const id=document.createElement("div")
-         id.setAttribute("class","id") 
-         id.innerHTML=element.cod
-         
-         
+                
+                const fabricante= document.createElement("div");
+                fabricante.setAttribute("class", "fabricante");
+                fabricante.innerHTML = element.fabricante;
+                linhas.appendChild(fabricante);
 
-         const nome=document.createElement("div")
-         nome.setAttribute("class","nome") 
-         nome.value=element.nome
+                
+                const quantidade = document.createElement("div");
+                quantidade.setAttribute("class", "quantidade");
+                quantidade.innerHTML = element.qtda;
+                linhas.appendChild(quantidade);
 
-         const fabricante=document.createElement("div")
-         fabricante.setAttribute("class","fabricante") 
-         fabricante.innerHTML=element.fabricante
+                
+                const custo = document.createElement("div");
+                custo.setAttribute("class", "custo");
+                custo.innerHTML = element.custo;
+                linhas.appendChild(custo);
 
-         const quantidade=document.createElement("div")
-         quantidade.setAttribute("class","quantidade") 
-         quantidade.innerHTML=element.qtda
+                
+                const preco = document.createElement("div");
+                preco.setAttribute("class", "preco");
+                preco.innerHTML = element.preco;
+                linhas.appendChild(preco);
 
-         const custo=document.createElement("div")
-         custo.setAttribute("class","custo") 
-         custo.innerHTML=element.custo
+                
+                const descricao = document.createElement("div");
+                descricao.setAttribute("class", "descricao");
+                descricao.innerHTML = element.descri.slice(0,10)
+                linhas.appendChild(descricao);
 
-         const preco=document.createElement("div")
-         preco.setAttribute("class","preco") 
-         preco.innerHTML=element.preco
+                const data = document.createElement("div");
+                data.setAttribute("class", "data");
+                data.innerHTML = element.data.slice(0,19)
+                linhas.appendChild(data);
 
-         const descricao=document.createElement("div")
-         descricao.setAttribute("class","descricao") 
-         descricao.innerHTML=element.descri
+                // Repeat the process for other elements...
 
-         const data=document.createElement("div")
-         data.setAttribute("class","data") 
-         data.innerHTML=element.data
-         
+                
+                linhas_prod.appendChild(linhas);
+                console.log(linhas_prod)
+              
+                }  
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
 
-         linhas.setAttribute("class","dt")
-         linhas.appendChild(id)
-         linhas.appendChild(nome)
-         linhas.appendChild(fabricante)
-         linhas.appendChild(quantidade)
-         linhas.appendChild(custo)
-         linhas.appendChild(preco)
-         linhas.appendChild(descricao)
-         linhas.appendChild(data)
-         linhas_prod.appendChild(linhas)
-
-         console.log(id,nome,fabricante)
-
-})}
-consultar()
-
-})
-
-    
+    consultar();
+});
